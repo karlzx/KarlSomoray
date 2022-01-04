ea53733db64cb0c04f6c559766d04290f06d883a
karlzx <karlsomoray@gmail.com>
2022-01-04 11:48:05 +1000

    Changes to main page

--- helperfns.py
+++ helperfns.py
@@ -7,25 +7,29 @@ import pydeck as pdk
 
 class ExperienceHandler():
     def __init__(self):
-        self.tags = pd.Series(['All'])
+        self.exptags = pd.Series([])
+        self.voltags = pd.Series([])
         self.experiences = []
+        self.vol_experiences = []
         self.education = []
     
-
-
     def add_experience(self,jobtitle, fromdate, todate, company, description, tags):
         self.experiences.append( self.Experience(jobtitle, fromdate, todate, company, description,tags))
 
-        self.tags = self.tags.append(pd.Series(tags), ignore_index= True)
-        # self.tags = self.tags.unique
+        self.exptags = self.exptags.append(pd.Series(tags), ignore_index= True)
+        self.exptags = pd.Series(self.exptags.unique())
 
-        # self.tags.append(, ignore_index = True)
-        # self.tags = self.tags.unique()
-        self.tags = pd.Series(self.tags.unique())
+    def add_vol_experience(self,jobtitle, fromdate, todate, company, description, tags):
+        self.vol_experiences.append( self.Experience(jobtitle, fromdate, todate, company, description,tags))
+
+        self.voltags = self.voltags.append(pd.Series(tags), ignore_index= True)
+        self.voltags = pd.Series(self.voltags.unique())
     
     def write_tags(self):
-        self.tags_chosen = st.multiselect('Select Experience Categories', 
-           self.tags, default = 'All')
+        self.tags_chosen = st.multiselect('Filter Experience Categories', self.exptags)
+
+    def write_vol_tags(self):
+        self.tags_chosen_vol = st.multiselect('Filter Experience Categories', self.voltags)
 
 
     def add_education(self,study, from_date, to_date, uni, description):
@@ -43,13 +47,23 @@ class ExperienceHandler():
 
     def write_experience(self):
         for exp in self.experiences:
-            if any(x in self.tags_chosen for x in exp.tags) or 'All' in self.tags_chosen:
+            if any(x in self.tags_chosen for x in exp.tags) or len(self.tags_chosen) == 0:
                 st.markdown('**{0}** - _{1} - {2}_  \n{3}'.format(exp.jobtitle, exp.from_date, exp.to_date, exp.company))
                 st.write("""{0}""".format(exp.description) )
                 st.caption("""_Tags: {0}_""".format(', '.join([str(elem) for elem in exp.tags])))
     
                 
                 st.write('_____________________________________________________')
+
+    def write_vol_experience(self):
+            for vol in self.vol_experiences:
+                if any(x in self.tags_chosen_vol for x in vol.tags) or len(self.tags_chosen_vol) == 0:
+                    st.markdown('**{0}** - _{1} - {2}_  \n{3}'.format(vol.jobtitle, vol.from_date, vol.to_date, vol.company))
+                    st.write("""{0}""".format(vol.description) )
+                    st.caption("""_Tags: {0}_""".format(', '.join([str(elem) for elem in vol.tags])))
+        
+                    
+                    st.write('_____________________________________________________')
 
 
     class Experience():
@@ -127,3 +141,32 @@ def prefill_experiences(eH):
     > * Environmental Engineering and Renewable Energies
     > * German (Beginner)
     """)
+
+    eH.add_experience("Retail Assistant", "Dec 2015", "Nov 2018", "City Beach Australia", """
+    > Quickly responded to manager’s requests and maintained a good relationship with other team members.
+
+    > Worked efficiently in various roles (as a general floor assistant, stock processor, counter staff, and salesannouncer) leading to reduced workloads for the following staff.
+    """, ['Retail', 'Customer Facing'])
+    
+    eH.add_vol_experience("President (Past: Secretary, Industry Team Leader/Member)", "Jan 2018", "Dec 2021", "QUT Electrical Engineering Student Society (QUT EESS)", """
+    > Overseeing of club operations, identifying gaps or oversights in club operations, liaising with university staff,sponsors and other industry partners, and communicating with all executive staff to gather feedback.
+    
+    > Developed  skills  in  conflict  resolution,   project  management,   team  management  and  eventplanning/facilitation.
+    """, ['Leadership', 'Event Planning', 'Team Management', 'Public Speaking'])
+
+    eH.add_vol_experience("Social Director", "Nov 2019", "Oct 2020", "QUT Mechanical Engineering Student Society (QUT MESS)", """
+    > Organized and directed a small team in hosting social events throguhout the semester.
+
+    > Managed communications and requests on Facebook page from internal teams and external parties.
+    
+    > Oversaw improvements in social media interaction and directed sold-out events.
+    """, ['Leadership', 'Event Planning', 'Team Management'])
+
+    eH.add_vol_experience("Social Director", "Feb 2017", "Jun 2018", "QUT STIMulate", """
+    > Provided one-on-one support for subjects regarding engineering and mathematics topics at ‘drop-in’ helpspace.
+    """, ['Teaching'])
+
+    eH.add_vol_experience("Connector", "Feb 2017", "Jun 2018", "QUT Connect", """
+    > Provide onboarding support for incoming students during Orientation Week.
+
+    """, ['Teaching', 'Public Speaking'])
--- main.py
+++ main.py
@@ -4,36 +4,77 @@ import pandas as pd
 import numpy as np
 
 import pydeck as pdk
+from PIL import Image
 
+st.set_page_config(layout="centered")
 ### BACKEND STUFF
 eH = hf.ExperienceHandler()
 hf.prefill_experiences(eH)
-### DISPLAY
-options = ('Home', 'Experience', 'Projects - Europe Trip Data')
 
+
+### DISPLAY
+
+## SIDEBAR
+options = ('Home', 'Experience', 'Education' , 'Projects - Europe Trip Data')
 page = st.sidebar.selectbox('Please select a page',options)
 st.sidebar.write('LinkedIn: in/karlsomoray/')
 
+
+## PAGES
+
 if page == options[0]:
-    st.title('Hi! My name is Karl')
-    st.write('Engineering and Mathematics Graduate from QUT ')
+    col1, col2 = st.columns(2)
+    with col2:
+        image = Image.open('./Images/selfie.jpg')
+        st.image(image)
+        
+    with col1:
+        st.title('Karl Somoray')
+        st.write('Engineering and Mathematics Graduate from QUT ')
+        st.write("""Passionate about Mechatronics, Data Analytics and Simulation. Please browse through my page to view my past professional and volunteer work,
+         as well as some projects that I've worked on including Data, Engineering, Mathematics  \n  \nThis website was built from scratch using the Streamlit API""")
 
+    st.write('__________')
+    st.subheader("Coding Experience:")
+    col3, col4, col5 = st.columns(3)
+    with col3:
+        st.metric(label="C/Python", value="1 Year")
+    with col4:
+        st.metric(label="MATLAB", value="5+ Years")
+    with col5:
+        st.metric(label="C#", value="1 Year")
+    st.write('__________')
+    st.subheader("Technical Skills:")
+    st.write("**_Engineering_**")
+    st.write("3D/PCB Design, Robot Mechanics and Logic, Project Management and Reporting")
+    st.write("**_Mathematics_**")
+    st.write("Simulation, Algorithm Design, Environment Modelling")
+    st.write("**_Data Analytics_**")
+    st.write("Power BI, Automation, Data Visualisation")
+    st.write("**_Software Development_**")
+    st.write("Jira, GIT, Object-Oriented Programming, .NET")
+    st.write('__________')
 elif page == options[1]:
-    st.title('Education and Experience')
-    with st.expander("Click to view Education", expanded = True):
-        st.subheader("Education")
-        eH.write_education()
-        
+    st.title('Experience')
+    # col1, col2 = st.columns(2)
 
-    with st.expander("Click to view Experience", expanded = True):
+    # with col1: 
+    with st.expander("Click to view Professional Experience", expanded = True):
+        st.subheader('Professional Experience')
         eH.write_tags()
-        st.subheader('Experience')
         eH.write_experience()
+    with st.expander("Click to view Volunteer Experience", expanded = True):
+        st.subheader('Volunteer Experience')
+        eH.write_vol_tags()
+        eH.write_vol_experience()
         
-    
-
 elif page == options[2]:
+    st.subheader("Education")
+    eH.write_education()
+
+elif page == options[3]:
     st.title('Project - Europe Trip Data')
+    st.write("Work In Progress")
 
 
 
